89efdd83c6eddcb0beb215799e3fa9ea6fc3ce53
Shehryar1021 <kiranimtiaz1985@gmail.com>
2024-10-01 16:52:06 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+name = "Ali"
+age = "13"
+text = "congratulations"
+
+#PRINTING THEIR VARIABLES AND THEIR TYPES
+print("name:" ) ("name")
+
+print("Data type of name is", type("name"))
+print("age:" ) ("age")
+
+print("Data type of  age is", type("age"))
+print("text:" ) ("text")
+
+print("Data type of  text is", type("text"))
